f592963049a18601edb764662b2b8cf39e518980
Elemide Adetola Miracle <84957208+ICET4663@users.noreply.github.com>
2026-09-23 15:25:35 +0100

    fix(api): deduplicate serverless cold starts

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,25 @@
 import { createTrackoApp } from '../src/create-app';
 
 let cachedServer: any;
+let bootstrapPromise: Promise<any> | undefined;
+
+async function getServer() {
+  if (cachedServer) return cachedServer;
+
+  bootstrapPromise ??= createTrackoApp()
+    .then(({ app }) => {
+      cachedServer = app.getHttpAdapter().getInstance();
+      return cachedServer;
+    })
+    .catch((error) => {
+      bootstrapPromise = undefined;
+      throw error;
+    });
+
+  return bootstrapPromise;
+}
 
 export default async function handler(request: any, response: any) {
-  if (!cachedServer) {
-    const { app } = await createTrackoApp();
-    cachedServer = app.getHttpAdapter().getInstance();
-  }
-
-  return cachedServer(request, response);
+  const server = await getServer();
+  return server(request, response);
 }
